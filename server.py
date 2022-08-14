54ea50664b681cfcec3f74116f1da70983eeae55
oktbabs <35835776+oktbabs@users.noreply.github.com>
2022-08-14 17:34:52 +0100

    Update server.py

--- server.py
+++ server.py
@@ -6,6 +6,6 @@ PORT = 8890
 Handler = http.server.SimpleHTTPrequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-     print("Server is running at ", PORT)
-	 print("To test from dockerized version, please run http://localhost:<exposed port>")
-	 httpd.serve_forever()
+    print("Server is running at ", PORT)
+    print("To test from dockerized version, please run http://localhost:<exposed port>")
+    httpd.serve_forever()
